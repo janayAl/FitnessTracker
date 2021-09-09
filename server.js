dd09b3c5a48de4344681a5635635b41a076fcf8a
janayAl <jsallen298705@gmail.com>
2021-09-08 21:35:52 -0500

    corrected require spelling error on mongoose require in the server file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // add express middleware and connections in this file
 const express = require('express');
 const user = require('');
-const mongoose = rewuir('mongoose')
+const mongoose = require('mongoose')
 //which port are you running? 
 const port = process.env.port
 const app = express();
