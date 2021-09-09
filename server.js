eab6b07b164ee4f3b48cdcb4720a57e3bed2bf1e
janayAl <jsallen298705@gmail.com>
2021-09-08 22:00:35 -0500

    added additonal info i.e. middleware to the server.js file

--- server.js
+++ server.js
@@ -3,5 +3,22 @@ const express = require('express');
 const user = require('');
 const mongoose = require('mongoose')
 //which port are you running? 
-const port = process.env.port
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
