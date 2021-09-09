19cb65b81616342e03cf4f06b064994109c6fd58
janayAl <jsallen298705@gmail.com>
2021-09-09 08:49:58 -0500

    added variables to the api routes.js in the controller folder

--- controllers/apiroutes.js
+++ controllers/apiroutes.js
@@ -1,6 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
 //add routes for the workout database in this file and the other js files in the public folder
 
 //CRUD Methods  Create, Read, Update, Delete
 //CREATE WORKOUT, post
 //Update/add workout, put
 //read/view workouts, get
+
